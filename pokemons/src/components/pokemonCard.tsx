22c94f81b5f24e8f82ba35f7555e00e0da2bf2d0
Gaurav Singh <gaurav@ascentspark.com>
2023-04-13 16:51:32 +0530

    fixes done for deploy

--- pokemons/src/components/pokemonCard.tsx
+++ pokemons/src/components/pokemonCard.tsx
@@ -13,16 +13,17 @@ const PokemonCard = (props: any) => {
   return (
     <div className="container mx-auto">
       <div className='flex flex-wrap gap-4 justify-center items-center'>
-        {PokemonData && PokemonData.map((pokemon: any) => (
-          <div className="max-w-sm rounded overflow-hidden shadow-lg cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110" onClick={() => detailPageNavigation(pokemon.name)}>
+        {PokemonData && PokemonData.map((pokemon: any, i: any) => (
+          <div className="max-w-sm rounded overflow-hidden shadow-lg cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
+            onClick={() => detailPageNavigation(pokemon.name)} key={i+1}>
             <img className="w-60 h-60 " src={pokemon.image} alt="pokemons" />
             <div className="px-6 py-4">
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{pokemon.number}</span>
               <div className="font-bold text-xl mb-2">{pokemon.name}</div>
             </div>
             <div className="px-6 pt-4 pb-2">
-              {pokemon.types.map((type: string) => (
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 bg-rose-700/[.12] ...">{type}</span>
+              {pokemon.types.map((type: string, i: any) => (
+                <span key={i} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 bg-rose-700/[.12] ...">{type}</span>
               ))}
             </div>
           </div>
